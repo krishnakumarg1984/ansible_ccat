385cf662e1bd3ef9b12d58f25ebfc796022be49f
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2025-06-25 13:04:05 +0100

    updates to ansible-ccat for module03 playbooks

--- module03/playbooks/my_collection/plugins/filter/phone_format.py
+++ module03/playbooks/my_collection/plugins/filter/phone_format.py
@@ -2,9 +2,7 @@
 def phone_format(number):
     return f"({number[:3]}) {number[3:6]}-{number[6:]}"
 
+
 class FilterModule(object):
     def filters(self):
-        return {
-            'phone_format': phone_format
-        }
-
+        return {"phone_format": phone_format}
