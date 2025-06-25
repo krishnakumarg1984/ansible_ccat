05bf4577364d67ec18bdef34e3c9eb9e55d4635a
Krishnakumar <krishna.kumar@ucl.ac.uk>
2025-06-25 15:59:13 -0400

    facts caching

--- module03/playbooks/checkcache.py
+++ module03/playbooks/checkcache.py
@@ -4,8 +4,8 @@ import redis
 import json
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-key = 'ansible_facts' + 'kubuntu'
+key = 'ansible_facts' + 'ansible1'
 val = r.get(key)
 
 data = json.loads(val)
-print (data['ansible_memfree_mb'])
+print(data['ansible_memfree_mb'])
